6bb531d79ed886e446d3f92d207fc6c62de9e84b
Algirdas Dumskis <algirdasdum@gmail.com>
2020-08-04 01:01:17 +0300

    Index file with routes handling

--- public/index.php
+++ public/index.php
@@ -1,3 +1,31 @@
 <?php
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use App\Services\Routing\RouterFactory;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require __DIR__.'/../vendor/autoload.php';
+
+/** @var ContainerBuilder $container */
+$container = require_once __DIR__.'/../bootstrap/container.php';
+
+/** @var RouterFactory $routerFactory */
+$routerFactory = $container->get('routing.router_factory');
+$router = $routerFactory->make();
+$route = $router->getMatchingRoute($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+
+if(!$route) {
+    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+    return;
+}
+
+$controllerMethod = $route->getControllerMethod();
+
+$controller = $container->get($route->getController());
+$response = $controller->$controllerMethod();
+
+header('Content-type: application/json');
+echo json_encode(['data' => $response]);
